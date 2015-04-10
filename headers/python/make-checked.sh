fb0b59a2acbc3bd5b54fbcce168d6727fcca15a6
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2015-04-10 11:24:33 +0200

    Headers: add tools invocation variant written in POSIX shell.
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

---
+++ headers/python/make-checked.sh
@@ -0,0 +1,8 @@
+list="ADC CCMSR CRC CPMP DCAN DCC DMA DMM EFUSE EMIF ESM FLASH FLEX_RAY GIO HTU I2C IOMM LIN MDIO N2HET PBIST PLL PMM RTI RTP SCI TCR TCRAM VIM"
+
+for a in $list ; do
+   python clean_raw_data.py -i $a.txt -o $a.json
+   python make_header.py -i $a.json -o $a.txt -l licence.txt > ../headers/$a.h
+done
+
+
